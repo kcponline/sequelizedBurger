1c23444ac3c719164c0f3fdbf9a193ee716e6447
Kent <kcponline@outlook.com>
2016-10-15 17:37:58 -0700

    commit 01 - load orm burger files and set up for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+var mysql = require('mysql');										// 	when deploying to heroku jawsdb, must use jawsdb_url codes
+var connection; 													// 	var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) { 										// 	port: 3306,
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {		
+	connection = mysql.createConnection({
+		host : 'localhost',											// 	host: 'localhost',
+		user : 'root',												// 	user: 'root',
+		password : "pa55w0rd", 										// 	password: 'pa55w0rd',
+		database : 'burgers_db' 									// 	database: 'burgers_db'
+	});
+};	
 
 
 connection.connect(function(err) {
